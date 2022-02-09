6dd3311fab93d083bc7ff681fa01833cb73271fa
Yu Ricky <leoleo5994@gmail.com>
2022-02-09 16:54:56 +0800

    add prettier-plugin-tailwindcss and setting bundle's output folder

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,24 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(jpe?g|png|svg|gif)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.(css$)/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
